985fcef636294818cc0bc0dd69e8325aa9456b8c
LJS7628 <dlwotmd7628@naver.com>
2024-09-19 12:22:56 +0900

    .

--- Assets/Scripts/EnemyMovingComponent.cs
+++ Assets/Scripts/EnemyMovingComponent.cs
@@ -4,24 +4,27 @@ using UnityEngine;
 public class EnemyMovingComponent : MonoBehaviour
 {
 
-    public float moveRadius = 1f;  // 이동 반경
-    public float stopDistance = 0.5f;  // 도착 후 정지 거리
-    public float moveSpeed = 3f;  // 몬스터 이동 속도
-    public float waitTime = 1f;  // 도착 후 멈추는 시간
-    private Vector3 targetPosition;  // 목표 위치
-    private Vector3 moveDirection;  // 이동 방향 벡터
-    private bool isMoving = true;  // 이동 상태 확인
+    public float moveRadius = 1f; 
+    public float stopDistance = 0.5f;
+    public float chaseDistance = 15.0f;
+    public float moveSpeed = 3f; 
+    public float waitTime = 1f;  
+    private Vector3 targetPosition;  
+    private Vector3 moveDirection;  
+    private bool isMoving = true;
 
     private Animator animator;
     private HealthPointComponent healthPoint;
+    private GameObject player;
     private float animatorSpeed=2.0f;
 
     void Start()
     {
         animator = GetComponent<Animator>();
         healthPoint = GetComponent<HealthPointComponent>();
+        player = GameObject.Find("Player");
 
-        GetNewRandomPosition(transform.position);  // 첫 랜덤 위치 생성
+        GetNewRandomPosition(transform.position);  
         animator.SetFloat("SpeedY", animatorSpeed);
 
     }
@@ -30,53 +33,77 @@ public class EnemyMovingComponent : MonoBehaviour
     {
         
         animator.SetFloat("SpeedY", animatorSpeed);
-        if (isMoving & healthPoint.IsDead == false)
+        if (healthPoint.IsDead == false)
         {
-            
-            // 목표 위치로 이동
-            MoveTowardsTarget();
-            // 몬스터가 목표 지점에 도착했는지 체크
-            if (Vector3.Distance(transform.position, targetPosition) < stopDistance)
+
+            if (isMoving & isChasing(player) == false)
             {
-                // 도착했으면 잠시 멈추고 다시 움직이기 위한 Coroutine 실행
-                StartCoroutine(WaitAndMoveAgain());
+                GetDirection(targetPosition);
+                MoveTowardsTarget();
+
+                if (Vector3.Distance(transform.position, targetPosition) < stopDistance)
+                {
+
+                    StartCoroutine(WaitAndMoveAgain());
+                }
             }
+
+            else if (isChasing(player))
+            {
+                GetDirection(player.transform.position);
+                MoveTowardsTarget();
+            }
+
+            
 
         }
     }
 
-    // 일정 반경 내의 랜덤 위치 구하기
+    
     void GetNewRandomPosition(Vector3 currentPosition)
     {
-        // 2D 랜덤 좌표 구한 후, 이를 3D로 변환
+        
         Vector2 randomPoint2D = Random.insideUnitCircle * moveRadius;
         targetPosition = new Vector3(currentPosition.x + randomPoint2D.x, currentPosition.y, currentPosition.z + randomPoint2D.y);
         if(isMoveRange() == false)
             GetNewRandomPosition(transform.position);
-        // 목표 지점까지의 방향 계산
-        moveDirection = (targetPosition - transform.position).normalized;  // 방향 벡터 정규화
-        Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
-        transform.rotation = targetRotation;
+
+        GetDirection(targetPosition);
     }
 
-    // 목표 지점으로 이동
+    
     void MoveTowardsTarget()
     {
-        // 일정 속도로 목표 방향으로 이동
+        
         transform.position += moveDirection * moveSpeed * Time.deltaTime;
     }
 
-    // 도착 후 잠깐 멈췄다가 다시 움직이는 Coroutine
+    void GetDirection(Vector3 target) 
+    {
+        moveDirection = (target - transform.position).normalized;
+        Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+        transform.rotation = targetRotation;
+
+    }
+
+
     IEnumerator WaitAndMoveAgain()
     {
         animatorSpeed = 0.0f;
-        isMoving = false;  // 일단 멈춤
-        yield return new WaitForSeconds(waitTime);  // waitTime 만큼 기다림
+        isMoving = false; 
+        yield return new WaitForSeconds(waitTime);  
 
-        GetNewRandomPosition(transform.position);  // 새로운 랜덤 목표 위치 설정
-        isMoving = true;  // 다시 움직임
+        GetNewRandomPosition(transform.position);  
+        isMoving = true;  
         animatorSpeed = 2.0f;
 
+    }
+
+    bool isChasing(GameObject player) 
+    {
+        if(Vector3.Distance(transform.position, player.transform.position) < chaseDistance)
+            return true;
+        return false;
     }
 
     bool isMoveRange() 
